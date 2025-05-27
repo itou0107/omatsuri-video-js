88e017caa3b0e9bd129c7389ffc79619e78e9c4d
itou0107 <147010065+itou0107@users.noreply.github.com>
2025-05-27 17:06:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+  const helloDiv = document.createElement("div");
+  helloDiv.textContent = "hello";
+  document.body.insertBefore(helloDiv, document.body.firstChild);
+});
